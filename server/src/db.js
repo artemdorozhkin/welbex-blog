9a4c92e8b15e9e10536dd515441ecb1c97901ee9
artodoro <aa.dorozhkin@ya.ru>
2023-05-08 04:47:32 +0300

    fix db link

--- server/src/db.js
+++ server/src/db.js
@@ -2,16 +2,24 @@ import { Sequelize } from "sequelize";
 import config from "./common/config.js";
 import log from "./common/logging.js";
 
-const sequelize = new Sequelize(
-  config.get("POSTGRES_DB"),
-  config.get("POSTGRES_LOGIN"),
-  config.get("POSTGRES_PASSWORD"),
-  {
-    logging: (msg) => log.debug("database", msg),
-    dialect: "postgres",
-    host: config.get("POSTGRES_HOST"),
-    port: config.get("POSTGRES_PORT"),
-  }
-);
+const isProduction = process.env.NODE_ENV === "production";
+
+const sequelize = isProduction
+  ? new Sequelize(process.env.DB_LINK, {
+      dialect: "postgres",
+      protocol: "postgres",
+      dialectOptions: {}, //removed ssl
+    })
+  : new Sequelize(
+      config.get("POSTGRES_DB"),
+      config.get("POSTGRES_LOGIN"),
+      config.get("POSTGRES_PASSWORD"),
+      {
+        logging: (msg) => log.debug("database", msg),
+        dialect: "postgres",
+        host: config.get("POSTGRES_HOST"),
+        port: config.get("POSTGRES_PORT"),
+      }
+    );
 
 export default sequelize;
